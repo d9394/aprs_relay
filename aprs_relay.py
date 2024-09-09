bebdf135ec27dd9160c02658aa2152faaaee7efb
d9394 <9394952@gmail.com>
2024-09-09 14:06:47 +0800

    Update aprs_relay.py

--- aprs_relay.py
+++ aprs_relay.py
@@ -6,66 +6,59 @@ import Queue
 from time import sleep,ctime
 import re
 
-queue_size = 100  # 缓存队列大小
+queue_size = 1000  # 缓存队列大小
 data_queue = Queue.Queue(queue_size)
 
 RECEIVE_SERVER = ('0.0.0.0', 14581)  # 接收服务器
 APRSIS_SERVER1 = ('192.168.1.1', 14580)  # SERVER1，适用于server1_callsigns列表中的呼号
 APRSIS_SERVER2 = ('127.0.0.1', 14580)  # SERVER2，适用于其它呼号
-CALLSIGN = 'NOCALL-0'  # 替换为你的呼号
-PASSCODE = '13023'  # 替换为你的 APRS-IS 密码
-FILTER = 'filter b/N0CALL'  # APRS-IS 过滤器
+#CALLSIGN = 'NOCALL-0'  # 替换为你的呼号
+#PASSCODE = '13023'  # 替换为你的 APRS-IS 密码
+#FILTER = 'filter b/N0CALL'  # APRS-IS 过滤器
+#login_str = 'user {} pass {} vers PythonAPRS 1.0 {}\n'.format(CALLSIGN, PASSCODE, FILTER)
 
 # 定义需要转发到 SERVER1 的呼号列表
-server1_callsigns = ['BB1BB-9', 'BB1BB-5']
+server1_callsigns = ['BB1BB-7', 'BB2BB-3']
 
 def extract_callsign(aprs_data):
 	# 正则表达式匹配呼号
-	match = re.match(r'user (\S+)', aprs_data)
+	match = re.match(r'user (\S+) pass.*', aprs_data)
 	if match:
 		return match.group(1).upper()
 	return None
-	
-def connect_to_receive_server(server):
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock.bind(server)
-	return sock
-	
-def connect_to_aprsis_server(server, callsign, passcode, filter_str):
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	login_str = 'user {} pass {} vers PythonAPRS 1.0 {}\n'.format(callsign, passcode, filter_str)
-	sock.sendto(login_str.encode('utf-8'), server)
-	return sock
 
-def receive_data(sock):
+def receive_data():
+	# 连接到接收服务器
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.bind(RECEIVE_SERVER)
 	while True:
 		data, addr = sock.recvfrom(1024)
 		if not data:
-			break
-		print("Received: {}".format(data.strip()))
-		# 接收到数据后返回 'R'
-		sock.sendto(b'R', addr)
-		# 确保将数据放入队列，仅当队列未满时才放入
-		if not data_queue.full():
-			data_queue.put(data)
-		#print("Current queue size: {}".format(data_queue.qsize()))  # 打印当前队列大小
+			continue
+		print("%s Received: %s" % (ctime(), data.strip()))
+		callsign = extract_callsign(data)		# 从数据中提取呼号，假设呼号在数据的前面部分
+		if callsign :			#判断是否APRS包，不是的直接丢弃
+			# 接收到数据后返回 'R'
+			sock.sendto(b'R', addr)
+			# 确保将数据放入队列，仅当队列未满时才放入
+			if not data_queue.full():
+				data_queue.put((data , callsign))
+			#print("Current queue size: {}".format(data_queue.qsize()))  # 打印当前队列大小
 
-def forward_data(sock1, sock2, server1, server2):
-	sock1.settimeout(10)  # 仅为 sock1 设置超时
+def forward_data():
+	# 准备sock连接
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.settimeout(10)  # 为 sock 设置超时10秒
 	while True:
-		data = data_queue.get()
+		data, callsign = data_queue.get()
 		#print("Process data : %s" % data)
-		# 从数据中提取呼号，假设呼号在数据的前面部分
-		callsign = extract_callsign(data)
 		# 确定转发目标服务器
 		if callsign in server1_callsigns:
-			target_sock = sock1
-			target_server = server1
 			while True:
 				try:
-					target_sock.sendto(data, target_server)
-					print("{} Forwarded to {}: {}".format(ctime(), target_server, data.strip()))
-					response, addr = target_sock.recvfrom(1024)
+					sock.sendto(data, APRSIS_SERVER1)
+					print("%s Forwarded to %s" % (ctime(), APRSIS_SERVER1))
+					response, addr = sock.recvfrom(1024)
 					if b'R' in response:
 						break
 					else :
@@ -75,35 +68,55 @@ def forward_data(sock1, sock2, server1, server2):
 					print("%s No response, retrying in 10 seconds..." % (ctime()))
 					sleep(10)
 				except Exception as e:
-					print("%s, Forward failed with error: {}, retrying in 10 seconds...".format(ctime(),e))
+					print("%s, Forward failed with error: %s, retrying in 10 seconds..." % (ctime(),e))
 					sleep(10)
 		else:
-			target_sock = sock2
-			target_server = server2
 			try:
-				target_sock.sendto(data, target_server)
-				print("%s Forwarded to {}: {}".format(ctime(), target_server, data.strip()))
+				sock.sendto(data, APRSIS_SERVER2)
+				print("%s Forwarded to %s" % (ctime(), APRSIS_SERVER2))
 			except Exception as e:
-				print("%s Forward failed with error: {}".format(ctime(), e))
+				print("%s Forward failed with error: %s" % (ctime(), e))
 		data_queue.task_done()
 		#print("Current queue size: {}".format(data_queue.qsize()))  # 打印当前队列大小
 
-def main():
-	# 连接到接收服务器
-	receive_sock = connect_to_receive_server(RECEIVE_SERVER)
-	
-	# 连接到两个 APRS-IS 服务器
-	sock1 = connect_to_aprsis_server(APRSIS_SERVER1, CALLSIGN, PASSCODE, FILTER)
-	sock2 = connect_to_aprsis_server(APRSIS_SERVER2, CALLSIGN, PASSCODE, FILTER)
-	
-	# 启动接收线程
-	threading.Thread(target=receive_data, args=(receive_sock,)).start()
-	
-	# 启动转发线程
-	threading.Thread(target=forward_data, args=(sock1, sock2, APRSIS_SERVER1, APRSIS_SERVER2)).start()
+## 线程状态管理
+threads = {}
+thread_targets = {
+	'receive_data': receive_data,
+	'forward_data': forward_data,
+}
 
-	# 防止主线程退出
-	data_queue.join()
+# 启动线程
+def start_thread(name, target):
+	thread = threading.Thread(target=target, name=name)
+	thread.setDaemon(True)  # 将线程设置为守护线程
+	thread.start()
+	threads[name] = thread
+	print("%s Starting %s thread" % (ctime(),name))
 
-if __name__ == "__main__":
-	main()
+# 检查线程状态
+def check_threads():
+	while True:
+		for name, thread in threads.items():
+			if not thread.is_alive():
+				#print("%s : %s is not alive. Restarting..." % (ctime(),name))
+				start_thread(name, thread_targets[name])
+		sleep(5)  
+
+if __name__ == '__main__':
+	# 使用循环启动所有线程
+	for name, target in thread_targets.items():
+		start_thread(name, target)
+	
+	# 启动线程检查
+	check_thread = threading.Thread(target=check_threads)
+	check_thread.setDaemon(True)  # 将线程设置为守护线程
+	check_thread.start()
+	
+	# 主线程保持运行
+	try:
+		while True:
+			sleep(10)
+	except KeyboardInterrupt:
+		print("Shutting down...")
+
